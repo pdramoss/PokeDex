65c114f3a47dba68c13884302cd395a9de55916f
Pedro Ramos <pdramoss@gmail.com>
2019-11-11 17:15:24 -0500

    Create BaseViewController

---
+++ PokeDex/Modules/BaseViewController.swift
@@ -0,0 +1,40 @@
+//
+//  BaseViewController.swift
+//  PokeDex
+//
+//  Created by Pedro Ramos on 11/11/19.
+//  Copyright © 2019 Pedro Ramos. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    private let imageView: UIImageView = UIImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func setBackgroundImage(image: UIImage?) {
+        if let first = view.subviews.first(where: { $0 === self.imageView.self }) as? UIImageView{
+            first.image = image
+            return
+        }
+        
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            imageView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        
+        imageView.image = image
+    }
+    
+    func removeBackgroundImage() {
+        imageView.removeFromSuperview()
+    }
+}
